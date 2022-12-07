d5cbd13b3bb651cbf918c89a252b4fbc2c96f95b
issei.hayashi <issei.hayashi@dc.kyocera.com>
2022-12-07 10:23:00 +0900

    l

--- mindmap.py
+++ mindmap.py
@@ -12,15 +12,14 @@ from zipfile import ZipFile
 import tempfile
 import os, glob
 from pymagnitude import Magnitude, MagnitudeUtils
-import matplotlib.font_manager as mpl
+import matplotlib.font_manager 
 
 NLP_PATH = r"%USERPROFILE%\Desktop\synonym.exe"
 # NLP_PATH = r"\\sd-ai-cpu01\k220415243\WORK\synonym.exe"
 
 def main():
 
-    mpl._rebuild()
-    print("設定ファイルの場所\n\t", mpl.matplotlib_fname())
+    raise (print(matplotlib.font_manager.findSystemFonts()))
     # ------------------------------- サイドバー -------------------------------
     input_text_main = st.sidebar.text_area('入力画面',height=350, key='input')
     
